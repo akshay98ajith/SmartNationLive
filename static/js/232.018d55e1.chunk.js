4870c85497f7719e75f0952957090ca92349bf58
Akshay Ajith <akshay98aji@gmail.com>
2023-01-02 15:42:29 +0400

    Updates

---
+++ static/js/232.018d55e1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksmart_nation=self.webpackChunksmart_nation||[]).push([[232],{2232:function(n,t,r){r.r(t),r.d(t,{default:function(){return a}});r(2791);var u=r(184),a=function(){return(0,u.jsx)(u.Fragment,{})}}}]);
+//# sourceMappingURL=232.018d55e1.chunk.js.map
